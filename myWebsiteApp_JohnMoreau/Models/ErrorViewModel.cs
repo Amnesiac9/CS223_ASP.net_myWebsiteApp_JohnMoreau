bd974143ddaef3e06029db125bcadcc5c98d9fff
John <johnmoreau19@gmail.com>
2023-11-11 17:38:43 -0800

    Add project files.

---
+++ myWebsiteApp_JohnMoreau/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace myWebsiteApp_JohnMoreau.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ myWebsiteApp_JohnMoreau/Program.cs
@@ -0,0 +1,41 @@
+/*
+* John Moreau
+* CSS233
+* 11/10/2023
+*
+*
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddRouting(options => {
+    options.LowercaseUrls = true;
+    options.AppendTrailingSlash = true;
+
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}/{slug?}"
+);
+
+app.Run();
